6e14d9ef3b2616d05b4d2f47e12f1a04135f3cce
Hudson Santos de Moraes <dinhogugu_@hotmail.com>
2024-04-18 16:54:20 -0300

    codigo simples para pipeline

---
+++ soma.php
@@ -0,0 +1,11 @@
+<?php
+
+Class Soma {
+
+    public function somar(int $a, int $b): string
+    {
+        return $a + $b;
+    }
+}
+
+?>
---
+++ somaTest.php
@@ -0,0 +1,23 @@
+<?php
+require 'soma.php';
+
+Class testSoma {
+
+    public function testSoma(): string
+    {
+        $class = new Soma();
+
+        if ($class->somar(5,8) == 10) {
+            return 'aeeee isso mesmo, deu bom!';
+        }
+
+        return 'deu ruim demais';
+    }
+}
+
+$classTest = new testSoma();
+
+var_dump($classTest->testSoma());
+
+
+?>
